b06e963de0fce24d8d021720f331342eaa958e42
Vince13580 <vincent.thouvenel@etu.univ-amu.com>
2019-09-13 11:20:03 +0200

    td1

--- TD1.php
+++ TD1.php
@@ -4,26 +4,20 @@
   <meta charset="utf-8">
   <title>Titre de la page</title>
 
-
 </head>
 <body>
-
-
 <?php
 echo \'<strong> Voici mon premier programme PHP </strong><br/>\' . "\n";
 echo \'C\\'est pas mal non ? <br />\';
-
+?>
+<?php
 $var1 =6;
 $var2= =1.3;
 $var3 =\'Variable 3\';
-echo "$var1 + $var2";
-echo $var1+$var2; ?>
-
+?>
+<?php echo "$var1 + $var2";?>
+<?php echo $var1+$var2; ?>
 </body>
-
-
-
-
 </html>
 
 
