cd4bec0fb7bd2ca53fa346c4c3ee65edf5916d76
Vince13580 <vincent.thouvenel@etu.univ-amu.com>
2019-09-13 09:48:03 +0200

    td1

---
+++ TD1.php
@@ -0,0 +1,28 @@
+<?php
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+
+
+</head>
+<body>
+
+
+
+echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
+echo 'C\'est pas mal non ? <br/>';
+
+$var1 =6;
+$var2= =1.3;
+$var3 ='Variable 3';
+echo "$var1 + $var2";
+echo $var1+$var2;
+</body>
+
+
+
+
+</html>
+?>
