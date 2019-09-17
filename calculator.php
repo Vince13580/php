c9af847d16266f37a78b97d8ead60b66d5ff27bc
t18003855 <thouvenel.vincent@gmail.com>
2019-09-17 15:06:58 +0200

    début ex4 partie calcul

--- calculator.php
+++ calculator.php
@@ -5,6 +5,10 @@ start_page("calculatrice");
  <form action="calcul.php" method="get">
             <input type="text" name="op1" value="num 1">
             <input type="text" name="op2" value="num 2"></br>
+     <inputchecked="checked"type="radio"name="op"value="*"/>*<br/>
+     <inputtype="radio"name="op"value="+"/>+<br/>
+     <inputtype="radio"name="op"value="-"/>-<br/>
+     <inputtype="radio"name="op"value="/"/>/<br/>
 
             <?php
                 for($cpt = 0 ; $cpt <= 3 ; ++$cpt)
