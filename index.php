fbf998772d617ffb2bf9fad9859ce4e9a2e4eafb
t18003855 <thouvenel.vincent@gmail.com>
2019-09-17 13:58:20 +0200

    tp1 ex3 avec le bon nom

--- index.php
+++ index.php
@@ -11,22 +11,10 @@ echo '<body><hr/><br/><strong>Test</strong><br/><hr/></body>' . PHP_EOL;
 };
 ?>
 <?php
-start_page('titre');
+start_page('test');
 end_page();
 ?>
 <body>
-<?php
-echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
-echo 'C\'est pas mal non ? <br />';
-?>
-<?php
-$var1 =6;
-$var2 =1.3;
-$var3 ='Variable 3';
-?>
-<?php echo "$var1 + $var2";?>
-<?php echo $var1+$var2; ?>
-<?php echo $var1 +$var3;?>
 </body>
 </html>
 
