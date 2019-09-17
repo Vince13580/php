501f83a3ac5ee2823bd5d9033a96410a72069842
t18003855 <thouvenel.vincent@gmail.com>
2019-09-17 15:20:27 +0200

    début ex4 partie calcul

--- calculator.php
+++ calculator.php
@@ -2,7 +2,7 @@
 include 'index.php';
 start_page("calculatrice");
 ?>
- <form action="calcul.php" method="get">
+ <form action="calcul.php" method="post">
      <input type="text" name="op1" value="num 1">
      <input type="text" name="op2" value="num 2"></br>
      <input checked="checked"type="radio"name="op"value="*"/>*<br/>
