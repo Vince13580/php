47dca47a270c689c168b6fccc7095ffb7e1497d9
t18003855 <thouvenel.vincent@gmail.com>
2019-09-17 15:03:06 +0200

    début ex4 partie calcul

--- calcul.php
+++ calcul.php
@@ -5,8 +5,18 @@ start_page("calcul");
 
 
 <?php
+$res = 0;
 $op1=$_POST['op1'];
 $op2=$_POST['op2'];
 $op=$_POST['op'];
+
+<?php
+if('*'==$op){
+
+}
+elseif('+'==$op){
+   $res = $op1+$op2;
+}
+else{echo'<br/><strong>opérateur'.$op.'nongéré</strong>';}?>
 ?>
 
