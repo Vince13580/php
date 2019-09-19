d2baaf79e27e74c4f157a336dbb5b05c9a8efeb0
t18003855 <thouvenel.vincent@gmail.com>
2019-09-19 10:08:21 +0200

    début ex5

--- calculator.php
+++ calculator.php
@@ -3,6 +3,19 @@ include 'index.php';
 start_page("calculatrice");
 ?>
  <form action="calcul.php" method="post">
+     <?php$operateurs='*+-/';
+
+     for($cpt=0;$cpt<=3;++$cpt)
+     {
+         echo'<input';
+         if($cpt==0)
+         {
+             echo'checked="checked"';
+         }
+         echo'type="radio"name="op"value="'.$operateurs[$cpt].'"/>'.$operateurs[$cpt].'<br/>'."\n";
+     }
+     ?>
+
      <input type="text" name="op1" value="num 1">
      <input type="text" name="op2" value="num 2"></br>
      <input checked="checked"type="radio"name="op"value="*"/>*<br/>
