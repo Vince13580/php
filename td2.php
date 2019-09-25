db7c3f2f947e742789d7063e8035d35130d58a89
t18003855 <thouvenel.vincent@gmail.com>
2019-09-25 16:14:19 +0200

    td4 ex4.2 test intermédiaire

--- td2.php
+++ td2.php
@@ -7,20 +7,20 @@ start_page('TD2');
 end_page();
 ?>
 <form action="data-processing.php" method="post">
-    <input  type="text" name="Identifiant" value="identifiant"/>
+    <input  type="text" name="Identifiant" value="identifiant"/><br>
     <input  type="radio" name="Civilité" value="M"/>
     <input  type="radio" name="Civilité" value="F"/>
-    <input  type="text" name="E-mail" VALUE="Adresse Email"/>
-    <input  type="password" name="Mot de passe"value="mot de passe"/>
-    <input  type="password" name="Vérification mot de passe" value="vérification mdp"/>
-    <input  type="text" name="Téléphone" value="numéro de téléphone"/>
-    <select name="Pays">
+    <input  type="text" name="E-mail" VALUE="Adresse Email"/><br>
+    <input  type="password" name="Mot de passe"value="mot de passe"/><br>
+    <input  type="password" name="Vérification mot de passe" value="vérification mdp"/><br>
+    <input  type="text" name="Téléphone" value="numéro de téléphone"/><br>
+    <select name="Pays"><br>
         <option value='France'>France</option>
         <option value='Slovénie'>Slovénie</option>
         <option value='Italie'>Italie</option>
     </select>
-    <input type="checkbox" name="conditions générales"/>
-    <input type="submit" name ="action" value="mailer"/>
+    <input type="checkbox" name="conditions générales"/><br>
+    <input type="submit" name ="action" value="mailer"/><br>
 
 
 
